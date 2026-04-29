c0994b3093bda616276e65fd57b6447e8bd81cda
1220769 <1220769@isep.ipp.pt>
2026-04-29 09:06:58 +0100

    fix: nodemailer Outlook SMTP

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,10 +12,16 @@ app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.office365.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    ciphers: 'SSLv3',
+    rejectUnauthorized: false,
   },
 })
 
